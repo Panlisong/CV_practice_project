41de8940eca1610bb8a2a73f39b23aad9e1d82e5
Bruno <43718470+Panlisong@users.noreply.github.com>
2020-01-31 18:13:50 +0800

    Correct the wrong code in the first time

--- project1/code/train_net.py
+++ project1/code/train_net.py
@@ -1,27 +1,51 @@
-import net
+import torch.nn as nn
+from torch.autograd import Variable
 import torch
-import data_preprocessing
-import numpy as np
-from cv2 import cv2
+import torch.utils.data as Data
+import torchvision
+import load_data
+import net
 
+EPOCH = 5
+BATCH_SIZE = 8
+LR = 0.01
 
-def predict(image):
-    '''
-    Return the num of the parking space in the image
+train_image, train_label = load_data.load_train_data()
+train_image, train_label = torch.Tensor(train_image), torch.Tensor(train_label)
 
-    Paramaters
-    ----------
-    image:
-            the image wait to be recognized
-    '''
-    parking_sapce_num, parking_space = data_preprocessing.crop(image)
+train_data = Data.TensorDataset(train_image, train_label)
+train_loader = Data.DataLoader(
+    dataset=train_data,
+    batch_size=BATCH_SIZE,
+    shuffle=True
+)
 
-    parking_space = np.reshape(parking_space, (parking_sapce_num, 3, 60, 30))
-    parking_space = torch.Tensor(parking_space)
+car_detection = net.car_detection_net()
+optimizer = torch.optim.Adam(car_detection.parameters(), LR)
+loss_function = nn.CrossEntropyLoss()
 
-    car_detection = net.car_detection_net()
-    car_detection.load_state_dict(torch.load('project1\\paramaters.pth'))
+for epoch in range(EPOCH):
+    LR /= 10
+    optimizer = torch.optim.Adam(car_detection.parameters(), LR)
+    whole_loss = 0
+    for step, (x, y) in enumerate(train_loader):
+        b_x = Variable(x)
+        b_y = Variable(y)
 
-    res = car_detection(parking_space)
-    res = np.argmax(res.data.numpy(), axis=1)
-    return np.sum(res)
+        output = car_detection(b_x)
+        loss = loss_function(output, b_y.long())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        whole_loss += loss.data.numpy()
+
+    whole_loss /= step+1
+    print(whole_loss)
+
+torch.save(car_detection.state_dict(), 'project1\\Parameters.pth')
+
+test_image, test_label = load_data.load_test_data()
+test_image, test_label = torch.Tensor(test_image), torch.Tensor(test_label)
+pred_y = car_detection(test_image)
+_, pred_y = torch.max(pred_y, dim=1)
+print(torch.sum(pred_y == test_label).numpy()/pred_y.shape[0])
